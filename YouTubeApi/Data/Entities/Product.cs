543da9f2ef2739a10cdcc7bfa9e8eabf0add806e
Manoj Pant <pantmanoj2005@gmail.com>
2024-11-22 21:59:58 +0530

    Add project files.

---
+++ YouTubeApi/Data/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YouTubeApi.Data.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }  
+        public decimal PriceTotal { get; set; }
+        public string Address { get; set; }
+    }
+}
